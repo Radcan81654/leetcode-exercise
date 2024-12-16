09decfa5143db18a03a73f12ec8eac4a3fcba5e8
Radcan81654 <laob81654@gmail.com>
2024-12-16 16:25:28 +0800

    翻转二叉树，对称二叉树

---
+++ 翻转二叉树，对称二叉树/2024.12.16.cpp
@@ -0,0 +1,61 @@
+//翻转二叉树
+//https://leetcode.cn/problems/invert-binary-tree/submissions/587411083/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root)
+            return root;
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+
+    }
+};
+
+
+
+//对称二叉树
+//https://leetcode.cn/problems/symmetric-tree/submissions/587414410/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool _isSymmetric(struct TreeNode* root1, struct TreeNode* root2)
+    {
+        if (root1 == NULL && root2 == NULL)
+            return true;
+        if (root1 == NULL || root2 == NULL)
+            return false;
+        if (root1->val != root2->val)
+            return false;
+        return _isSymmetric(root1->left, root2->right) && _isSymmetric(root1->right, root2->left);
+    }
+    bool isSymmetric(struct TreeNode* root)
+    {
+        return _isSymmetric(root->left, root->right);
+    }
+};
