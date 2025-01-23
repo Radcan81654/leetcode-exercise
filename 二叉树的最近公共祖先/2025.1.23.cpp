1568649e55b389809854bfe77b8efb420c95ba4d
Radcan81654 <laob81654@gmail.com>
2025-01-23 11:41:39 +0800

    二叉树的最近公共祖先

---
+++ 二叉树的最近公共祖先/2025.1.23.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/description/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr)
+            return nullptr;
+        if (root == p || root == q)
+            return root;
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        if (l && r)
+            return root;
+        if (!l && r)
+            return r;
+        if (l && !r)
+            return l;
+        //到这只剩下两边都是null的情况
+        return nullptr;
+
+
+
+
+    }
+};
